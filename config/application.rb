c56bf8ee21b10b3daa7a8d9b20ab003bb78705f0
Dylan Yang <email": null>
2017-07-25 18:10:02 -0400

    Rack-cors debug

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,11 @@ module KAWGDBe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
+    'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept'
+    }
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
