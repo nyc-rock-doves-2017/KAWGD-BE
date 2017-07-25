ada04ea31b54c7d7d388d3c518e650362e6912ae
Dylan Yang <email": null>
2017-07-25 19:09:56 -0400

    Enable sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :orders, except: [:destroy]
-  # resources :sessions, only: [:index]
+  resources :sessions, only: [:index, :create]
   post '/login', to: 'sessions#create'
 
   root to: 'sessions#index'
